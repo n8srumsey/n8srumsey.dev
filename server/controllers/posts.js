29e03bb95807d1171e374ce2bbec10c8ba15afdc
Nathan Rumsey <nathan.s.rumsey@gmail.com>
2021-07-13 15:53:39 -0700

    Finished half of MERN tutorial

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,27 +1,26 @@
-import PostMessage from '../models/postMessage.js'
+import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
-    try {
-        const postMessages = await PostMessage.find();
+  try {
+    const postMessages = await PostMessage.find();
 
-        console.log(postMessages)
+    console.log(postMessages);
 
-        res.status(200).json(postMessages)
-    } catch (error) {
-        res.status(404).json({message: error.message})
-    }
-    
-}
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 export const createPost = async (req, res) => {
-    const post = req.body;
-    
-const newPost = newPostMessage(post);
+    const { title, message, selectedFile, creator, tags } = req.body;
+
+    const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
 
     try {
-        await newPost.save();
+        await newPostMessage.save();
 
-        res.status(201).json(newPost);
+        res.status(201).json(newPostMessage);
     } catch (error) {
-        res.status(409).json({message: error.message});
+        res.status(409).json({ message: error.message });
     }
 }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import bodyParser from 'body-parser'; 
 import mongoose from 'mongoose';
 import cors from 'cors';
 
@@ -7,11 +6,11 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
-app.use(bodyParser.json({ limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(express.urlencoded({ limit: '25mb', extended: true }));
+app.use(express.json({ limit: '25mb' }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://n8srumsey:gULWVDTmumkkUzriRV33@n8srumsey-dev.epbfy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
