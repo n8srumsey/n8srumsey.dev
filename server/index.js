62aebc99d112e31d183c1ea7dc23308df38e9657
Nathan Rumsey <nathan.s.rumsey@gmail.com>
2021-08-02 20:06:15 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,6 +13,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get('/', (req, res) => {
+    res.send('This is the n8srumsey.dev API');
+})
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
