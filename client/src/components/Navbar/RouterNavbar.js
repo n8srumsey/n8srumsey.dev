78b0bbb3d4675374d12939f079c60b8c287e0383
Nathan Rumsey <nathan.s.rumsey@gmail.com>
2021-08-04 11:48:35 -0700

    Converted links from 'href' to 'to'

--- client/src/components/Navbar/RouterNavbar.js
+++ client/src/components/Navbar/RouterNavbar.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link as RouterLink } from 'react-router-dom';
 import { Typography, AppBar, Toolbar, withStyles, MenuItem, Menu, IconButton, Link, useTheme } from "@material-ui/core";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import MenuIcon from "@material-ui/icons/Menu";
 import useStyles from "./styles";
+
 
 const SiteName = withStyles({
   root: {
@@ -40,16 +42,16 @@ const SiteNavbar = (props) => {
       className={classes.root}
     >
       <MenuItem>
-        <Link href='/' className={classes.mobileMenu}>About</Link>
+        <Link to='/' component={RouterLink} className={classes.mobileMenu}>About</Link>
       </MenuItem>
       <MenuItem>
-        <Link href='/projects/' className={classes.mobileMenu}>Projects</Link>
+        <Link to='/projects/' component={RouterLink} className={classes.mobileMenu}>Projects</Link>
       </MenuItem>
       <MenuItem>  
-        <Link href='/resume/' className={classes.mobileMenu}>Resume</Link>
+        <Link to='/resume/' component={RouterLink} className={classes.mobileMenu}>Resume</Link>
       </MenuItem>
       <MenuItem>
-        <Link href='/blog/' className={classes.mobileMenu}>Blog</Link>
+        <Link to='/blog/' component={RouterLink} className={classes.mobileMenu}>Blog</Link>
       </MenuItem>
       <MenuItem>  
         <Link href='https://github.com/n8srumsey' className={classes.mobileMenu}><GitHubIcon /></Link>
@@ -67,10 +69,10 @@ const SiteNavbar = (props) => {
       <SiteName edge="start" variant="h5" className={classes.title} noWrap >n8srumsey.dev</SiteName> 
         <div className={classes.grow} />
         <div className={classes.sectionDesktop}>
-          <Link href='/' className={classes.NavbarLink} variant="h6">About</Link>
-          <Link href='/projects/' className={classes.NavbarLink} variant="h6">Projects</Link>
-          <Link href='/resume/' className={classes.NavbarLink} variant="h6">Resume</Link>
-          <Link href='/blog/' className={classes.NavbarLink} variant="h6">Blog</Link>
+          <Link to='/' className={classes.NavbarLink} component={RouterLink} variant="h6">About</Link>
+          <Link to='/projects/' className={classes.NavbarLink} component={RouterLink} variant="h6">Projects</Link>
+          <Link to='/resume/' className={classes.NavbarLink} component={RouterLink} variant="h6">Resume</Link>
+          <Link to='/blog/' className={classes.NavbarLink} component={RouterLink} variant="h6">Blog</Link>
           <Link href='https://github.com/n8srumsey' className={`${classes.NavbarLink} ${classes.NavbarIcon}`} variant="h6"><GitHubIcon /></Link>
           <Link href='https://www.linkedin.com/in/nathan-rumsey-66ab1320a/' className={`${classes.NavbarLink} ${classes.NavbarIcon}`} variant="h6"><LinkedInIcon /></Link>
         </div>
