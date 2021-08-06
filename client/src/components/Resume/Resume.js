5cf9f7fca83dd78a1d08cd086b25e351743cc3e1
Nathan Rumsey <nathan.s.rumsey@gmail.com>
2021-08-05 19:11:24 -0700

    Implement Resume

--- client/src/components/Resume/Resume.js
+++ client/src/components/Resume/Resume.js
@@ -1,29 +1,62 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef, useState } from 'react';
 
-import { Container, Grid, Grow, Typography } from "@material-ui/core";
+import { Button, Container, Grid, Grow, Link, Typography } from "@material-ui/core";
+import { Document, Page } from "react-pdf/dist/umd/entry.webpack";
+
 
 import useStyles from "./styles";
-import PDFViewer from './PDFViewer/PDFViewer';
+
+import GetApp from '@material-ui/icons/GetApp'
 
 const Resume = () => {
+    const [pdfTop, setPdfTop] = useState(0);
+    const [pdfRight, setPdfRight] = useState(0);
+    const [pdfDisplay, setPdfDisplay] = useState('none')
     const classes = useStyles();
+
+    const pdfReference = useRef(null);
+
+    const [pageNumber, setPageNumber] = useState(1); //setting 1 to show fisrt page
+
+    const updatePosition = () => {
+        setPdfTop(pdfReference.current.getBoundingClientRect().top);
+        setPdfRight(pdfReference.current.getBoundingClientRect().right)
+    }
+
+    function onDocumentLoadSuccess() {
+        setPageNumber(pageNumber + 1);
+        setTimeout(() => { updatePosition(); }, 10)
+        setPdfDisplay('flex');
+    }
+
+    useLayoutEffect(() => {
+        window.addEventListener('resize', updatePosition);
+        return () => window.removeEventListener('resize', updatePosition);
+    }, []);
 
     return (
         <Container>
-            <Container className={classes.container}>
-                <Grow in>
-                    <Container style={{ marginTop: "5em" }}>
-                        <Grid container justifyContent="center" alignItems="stretch" spacing={3} direction='column'>
-                            <Grid item>
-                                <Typography className={classes.heading} variant="h3">Resume</Typography>
-                            </Grid>
-                            <Grid item>
-                                <PDFViewer />
-                            </Grid>
-                        </Grid>
-                    </Container>
-                </Grow>
-            </Container>
+            <Grow in>
+                <Grid container className={classes.container} justifyContent="center" alignItems="center" spacing={3} direction='column'>
+                    <Grid item>
+                        <Typography className={classes.heading} variant="h3">Resume</Typography>
+                    </Grid>
+                    <Grid item>
+                        <div ref={pdfReference}>
+                            <Document
+                                file="resume.pdf"
+                                onLoadSuccess={onDocumentLoadSuccess}
+                            >
+                                <Page pageNumber={pageNumber} />
+                            </Document>
+                        </div>
+                        <Link href='/resume.pdf' download component={Button} className={classes.downloadButton} style={{ top: pdfTop + 15, left: pdfRight - 150, display: pdfDisplay }}>
+                            <GetApp />
+                            <Typography style={{ fontWeight: 'bold' }}>Download</Typography>
+                        </Link>
+                    </Grid>
+                </Grid>
+            </Grow>
         </Container>
     );
 };
--- client/src/components/Resume/styles.js
+++ client/src/components/Resume/styles.js
@@ -1,7 +1,18 @@
 import { makeStyles } from "@material-ui/core/styles";
+import * as COLORS from '../../constants/colors';
 
-export default makeStyles((theme) => ({
+export default makeStyles(() => ({
   grow: {
     flexGrow: 1,
   },
+  container: {
+    marginTop: "5em"
+  },
+  heading: {
+    color: COLORS.PRIMARY
+  },
+  downloadButton: {
+    position: 'absolute',
+    zIndex: 1,
+  }
 }));
